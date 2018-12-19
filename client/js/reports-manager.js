b5d0a82cdf1cb76d5bb154f3353c54ca50088b93
Chirag Kataria <chirag.kataria@jungleworks.com>
2018-12-19 11:54:00 +0530

    bug fix for inserting

--- client/js/reports-manager.js
+++ client/js/reports-manager.js
@@ -6896,7 +6896,7 @@ class ReportTransformations extends Set {
 			this.stage.load();
 
 			new SnackBar({
-				message: 'Transformation Added',
+				message: 'Transformation Deleted',
 				icon: 'far fa-save',
 			});
 
@@ -6927,8 +6927,12 @@ class ReportTransformation {
 
 		this.name = new type().name;
 
-		if(!this.options)
+		if(!this.options) {
 			this.options = {};
+		}
+		// else {
+		// 	this.options = JSON.parse(this.options);
+		// }
 	}
 
 	get container() {
@@ -7026,11 +7030,6 @@ class ReportTransformation {
 
 		container.querySelector('.actions .remove').on('click', () => this.deleteTransformation(this.id));
 
-		container.querySelector('.actions .remove').on('click', () => {
-			this.transformations.delete(this);
-			this.transformations.preview();
-		});
-
 		return container;
 	}
 
@@ -7045,7 +7044,7 @@ class ReportTransformation {
 			parameters = {
 				owner: 'visualization',
 				id: this.id,
-				options: JSON.stringify(this.json),
+				options: JSON.stringify(this.json.options),
 				type: this.type,
 				title: this.container.querySelector('.transformation-title').value,
 			};
