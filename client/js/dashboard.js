b5c6d2417ffb1ba3d687ee8065161890d7fc96be
shubhi.verma <shubhi7996@gmail.com>
2019-01-07 16:46:31 +0530

    report filters all value to null

--- client/js/dashboard.js
+++ client/js/dashboard.js
@@ -1,4 +1,4 @@
-Page.class = class Dashboards extends Page {
+class Dashboards extends Page {
 
 	constructor() {
 
@@ -611,7 +611,9 @@ Page.class = class Dashboards extends Page {
 
 		await Sections.show('reports');
 	}
-};
+}
+
+Page.class = Dashboards;
 
 class Dashboard {
 
