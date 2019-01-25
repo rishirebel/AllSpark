10dced0077f01343c5f8918c36a35173d7f82c55
Vivek Puri <vivek.puri@jungleworks.com>
2019-01-25 12:51:14 +0530

    removed companion repo from main

--- startpm2.sh
+++ startpm2.sh
@@ -1,28 +1,17 @@
 #!/bin/bash
 environment="$1"
-
+dbpass=`echo $MYSQLPASS`
 cd /apps/node-apps/allspark/
 cp config/sample.json config/$environment.json
 sed -i "s|8080|3001|g" config/$environment.json
 sed -i "s|mysql-host|mysql-local|g" config/$environment.json
 sed -i "s|mysql-read|root|g" config/$environment.json
 sed -i "s|mysql-write|root|g" config/$environment.json
-sed -i "s|mysql-pass|password|g" config/$environment.json
+sed -i "s|mysql-pass|$dbpass|g" config/$environment.json
 sed -i "s|env-db|"$environment"_allspark|g" config/$environment.json
 NODE_ENV="$environment" pm2 start bin/www --name "$environment"
 
-cd /apps/node-apps/allspark/kato-allspark-companion
-cp config/sample.json config/$environment.json
-sed -i "s|8080|8001|g" config/$environment.json
-sed -i "s|mysql-host|mysql-local|g" config/$environment.json
-sed -i "s|mysql-read|root|g" config/$environment.json
-sed -i "s|mysql-write|root|g" config/$environment.json
-sed -i "s|mysql-pass|password|g" config/$environment.json
-sed -i "s|env-db|"$environment"_allspark|g" config/$environment.json
-NODE_ENV="$environment" pm2 start bin/www --name "$environment-comp"
-
 sleep 20
-#below port is for allspark
 curl http://localhost:3001/api/v2/setup/run
 pm2 restart all
 
