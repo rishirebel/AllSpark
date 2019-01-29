8806ad85f88f4ef8eaedf56061424fa32d916a56
vivekpuri95 <vivekpuri1995@gmail.com>
2019-01-29 17:54:41 +0530

    Update startpm2.sh

--- startpm2.sh
+++ startpm2.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 environment="$1"
+dbhost=`echo $MYSQLHOST`
+dbuser=`echo $MYSQLUSER`
 dbpass=`echo $MYSQLPASS`
+
 cd /apps/node-apps/allspark/
 cp config/sample.json config/$environment.json
 sed -i "s|8080|3001|g" config/$environment.json
-sed -i "s|mysql-host|mysql-local|g" config/$environment.json
-sed -i "s|mysql-read|root|g" config/$environment.json
-sed -i "s|mysql-write|root|g" config/$environment.json
+sed -i "s|mysql-host|$dbhost|g" config/$environment.json
+sed -i "s|mysql-read|$dbuser|g" config/$environment.json
+sed -i "s|mysql-write|$dbuser|g" config/$environment.json
 sed -i "s|mysql-pass|$dbpass|g" config/$environment.json
 sed -i "s|env-db|"$environment"_allspark|g" config/$environment.json
 NODE_ENV="$environment" pm2 start bin/www --name "$environment"
